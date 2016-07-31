6542d8ce50abbe91d6ffa3f8212615c92c71159d
blazej <blaze@blazejs-macbook-pro.home>
2016-07-31 13:26:34 +0200

    chap2a

--- Ex2a_PeanoNat.hs
+++ Ex2a_PeanoNat.hs
@@ -3,7 +3,7 @@ module Ex2a_PeanoNat where
 -- Peano numbers represent the natural numbers, by taking them
 -- as either 0 or n+1. Doing anything with them is usually very
 -- inefficient.
-data PNat = Zero | Succ PNat 
+data PNat = Zero | Succ PNat
 
 p0, p1, p2, p3, p4 :: PNat
 
@@ -17,33 +17,42 @@ p4 = Succ p3
 -- Ex 2a.1: Implement Peano addition
 infixl 6 .+
 (.+) :: PNat -> PNat -> PNat
-(.+) Zero pn2 = undefined
-(.+) (Succ x) pn2 = undefined
+(.+) Zero pn2 = pn2
+(.+) (Succ x) pn2 = x .+ (Succ pn2)
 
+--(a + 1) * b = b + ab
 
 -- Ex 2a.2: Implement Peano multiplication
 infixl 7 .*
 (.*) :: PNat -> PNat -> PNat
-(.*) pn1 pn2 = undefined
+(.*) Zero pn2 = Zero
+(.*) (Succ pn1) pn2 = pn1 .* pn2 .+ pn2
+
+
+--a ^ (b+1)= a * a^b
 
 -- Ex 2a.3: Implement Peano exponential
 infixr 8 .^
 (.^) :: PNat -> PNat -> PNat
-(.^) pn1 pn2 = undefined
+(.^) pn1 Zero = Succ Zero
+(.^) pn1 (Succ x) = pn1 .* pn1 .^x
+
 
 -- Ex 2a.4: Convert a Peano number to an integer
 unpeano :: PNat -> Int
-unpeano = undefined
+unpeano Zero = 0
+unpeano (Succ x) = 1 + (unpeano x)
 
 -- Ex 2a.5: Convert a non-negative integer to a Peano number
 peano :: Int -> PNat
-peano = undefined
+peano 0 = Zero
+peano n = Succ $ peano (n-1)
 
 
 
 -- Instance boilerplate
 instance Show PNat where
   show = ("peano " ++) . show . unpeano
-  
-instance Eq PNat where 
-  a == b = unpeano a == unpeano b 
+
+instance Eq PNat where
+  a == b = unpeano a == unpeano b
